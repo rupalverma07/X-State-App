8e3489001db1a0a80b83008c65dd44b6cc195bf1
sonirupal <rupalsoni07@gmail.com>
2024-05-30 18:30:26 +0530

    test error fixed

--- src/components/countries/Country.jsx
+++ src/components/countries/Country.jsx
@@ -53,26 +53,26 @@ const Country = () => {
       <div className='container'>
         <div>
             <select value={country} onChange={countryHandler} className='formInput'>
-                <option value="">Select Country</option>
+                <option value="" disabled>Select Country</option>
                 {countriesData.length>0 ? (countriesData.map((item,i) => <option key={i} value={item}>{item}</option>)) : (null)}
             </select>
         </div>
         <div>
         <select value={state} onChange={stateHandler} className='formInput' disabled={statesData.length === 0}>
-        <option value="">Select State</option>
+        <option value="" disabled>Select State</option>
                 {statesData.length>0 ? (statesData.map((item,i) => <option key={i} value={item}>{item}</option>)) : (null)}
         </select>
         </div>
         <div>
         <select value={city} onChange={cityHandler} className='formInput' disabled={citiesData.length === 0}>
-        <option value="">Select City</option>
+        <option value="" disabled>Select City</option>
                 {citiesData.length>0 ? (citiesData.map((item,i) => <option key={i} value={item}>{item}</option>)) : (null)}
             </select>
         </div>
         
       </div>
       <div>
-            {showMsg ? (<h3>You selected {city} {state} {country}</h3>) : (null)}
+            {showMsg ? (<h3>You selected {city}, {state}, {country}</h3>) : (null)}
         </div>
     </div>
   )
